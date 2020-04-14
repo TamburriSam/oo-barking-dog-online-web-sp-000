5b0ab3a56a276805336714cf8dab53bf7fd6ebdb
Sam Tamburri <tamburrisam@gmail.com>
2020-04-14 16:21:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,15 @@
-class Dog 
-  
+class Dog
+
    def name=(dogs_name, dogs_bark = "woof!")
     @name = dogs_name
     @bark = dogs_bark
   end
-  
-  def name 
-    @name 
+
+  def name
+    @name
   end
-  
+
   def bark
     puts "woof!"
   end
 end
-  
- 
-
-
-  
